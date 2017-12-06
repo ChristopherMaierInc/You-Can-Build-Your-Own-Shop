a8247ec1bf9a84e571a236be525dd5820d309d31
mail@christophermaier.com.au <mail@christophermaier.com.au>
2017-12-06 11:51:01 +1000

    Express error handling

--- api/server.js
+++ api/server.js
@@ -1,3 +1,4 @@
+// Bring in our environment variables
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
@@ -9,8 +10,18 @@ const app = express()
 // Plugins
 app.use(bodyParser.json()); // Allows me to have JSON uploads (POST/PUT)
 
-// Routes
+// JSON error handling
+app.use((error, req, res, next) => {
+  res.send({ error: error.message })
+})
 
+app.use((req, res, next) => {
+  // No other routhes left, must be a 404!
+  res.status(404).send({
+    error: `no route found for ${ req.method } ${ req.url }` })
+})
+
+// Routes
 app.listen(7000, (error) => {
   if (error) {
     console.log('There was and problem starting the server', error)
